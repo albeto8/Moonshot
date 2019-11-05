8543d9cec77f6c475a103e107b96bf4a19156284
Mario Alberto Barragan Espinosa <mario.barragan@globant.com>
2019-11-05 09:48:16 -0600

    Initial Commit

---
+++ Moonshot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Mario Alberto Barragan Espinosa on 11/5/19.
+//  Copyright © 2019 Mario Alberto Barragan Espinosa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
