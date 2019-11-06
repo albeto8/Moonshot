a2859c71793a4569ede924a7047df8c9f5313695
Mario Alberto Barragan Espinosa <mario.barragan@globant.com>
2019-11-06 11:36:49 -0600

    Fixing problems with buttonStyle() and layoutPriority()

---
+++ Moonshot/AstronautView.swift
@@ -0,0 +1,21 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Mario Alberto Barragan Espinosa on 11/6/19.
+//  Copyright © 2019 Mario Alberto Barragan Espinosa. All rights reserved.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    static var previews: some View {
+        AstronautView()
+    }
+}
